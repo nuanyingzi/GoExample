41bd68482af8b5dc5ef71f62cc811c71ccd1ecc8
Tao <852947475@qq.com>
2022-09-17 22:13:29 +0800

    变量 常量

---
+++ const/const.go
@@ -0,0 +1,10 @@
+package main
+
+// 常量
+
+import "fmt"
+
+func main() {
+	const Pi float64 = 3.1415926
+	fmt.Println(Pi)
+}
---
+++ variable/variable.go
@@ -0,0 +1,37 @@
+package main
+
+// 变量
+
+import "fmt"
+
+func getNameAndAge() (string, int) {
+	return "大宝", 18
+}
+
+func main() {
+
+	// 初始化 赋初值
+	var name string = "dabao"
+	var age uint8
+	var married bool
+
+	fmt.Printf("name = %v, type = %T\n", name, name)
+	fmt.Printf("age = %v, type = %T\n", age, age)
+	fmt.Printf("married = %v, type = %T\n", married, married)
+
+	// 类型推断
+	var s = "string"
+	fmt.Printf("s = %v, type = %T\n", s, s)
+
+	// 短变量声明
+	shi := "是"
+	fmt.Printf("shi = %v, type = %T\n", shi, shi)
+
+	// 匿名变量
+	dName, dAge := getNameAndAge()
+	fmt.Println(dName, dAge)
+
+	dName1, _ := getNameAndAge()
+	fmt.Println(dName1)
+
+}
